f0fff63e3edb20f6b051a5b21394c583d65d58db
Hope <hopewanroy@gmail.com>
2016-12-02 15:55:15 -0800

    Update Associatable Module and AssocOptions Classes

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,102 +10,74 @@ class AssocOptions
   )
 
   def model_class
-    @name.to_s.singularize.camelcase.constantize
+    @class_name.constantize
   end
 
   def table_name
-    @name.to_s.singularize.camelcase.constantize.table_name
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     defaults = {
-      foreign_key: (name.to_s.underscore + "_id").to_sym,
+      foreign_key: "#{name}_id".to_sym,
       primary_key: :id,
       class_name: name.to_s.camelcase
     }
-    options = defaults.merge(options)
-    @name = name
-    @foreign_key = options[:foreign_key]
-    @primary_key = options[:primary_key]
-    @class_name = options[:class_name]
+
+    defaults.each do |key, _|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     defaults = {
-      foreign_key: (self_class_name.to_s.singularize.underscore + "_id").to_sym,
+      foreign_key: "#{self_class_name.underscore}_id".to_sym,
       primary_key: :id,
       class_name: name.to_s.singularize.camelcase
     }
-    options = defaults.merge(options)
-    @name = name
-    @self_class_name = self_class_name
-    @foreign_key = options[:foreign_key]
-    @primary_key = options[:primary_key]
-    @class_name = options[:class_name]
+
+    defaults.keys.each do |key, _|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name, options)
-    @@assoc_options[:belongs_to] = options
-    
-    define_method(:"#{name}") do
-      foreign_key = send("#{options.foreign_key}")
-      target_model_class = options.model_class
-      target_model_class.where(options.primary_key => foreign_key).first
-    end
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
 
+    define_method(name) do
+      options = self.assoc_options[name]
+      foreign_key = send(options.foreign_key)
+      options
+        .model_class
+        .where(options.primary_key => foreign_key)
+        .first
+    end
   end
 
   def has_many(name, options = {})
-    options = HasManyOptions.new(name, self, options)
 
-    define_method(:"#{name}") do
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.assoc_options[name]
       foreign_key = send("#{options.primary_key}")
-      target_model_class = options.model_class
-      target_model_class.where(options.foreign_key => foreign_key)
+      options
+        .model_class
+        .where(options.foreign_key => foreign_key)
     end
   end
 
   def assoc_options
-    @@assoc_options = {}
-    DBConnection.execute(<<-SQL)
-      SELECT
-        *
-      FROM
-        #{self.table_name}
-      JOIN
-
-      WHERE
-    SQL
+    @assoc_options ||= {}
+    @assoc_options
   end
-
-#  has_one_through is going to need to make a join query that uses
-#  and combines the options (table_name, foreign_key, primary_key)
-#  of the two constituent associations. This requires us to store
-#  the options of a belongs_to association so that has_one_through
-#  can later reference these to build a query.
-#
-# Modify your 03_associatiable.rb file and implement the
-# ::assoc_options class method. It lazily-initializes a class
-# instance variable with a blank hash. Modify your belongs_to
-# method to save the BelongsToOptions in the assoc_options hash,
-# setting the options as the value for the key name
-#
-#   SELECT
-#   houses.*
-# FROM
-#   humans
-# JOIN
-#   houses ON humans.house_id = houses.id
-# WHERE
-#   humans.id = ?
 end
 
 class SQLObject
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,6 +5,19 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+
+
+
+# Lookup through_name in assoc_options; call this 
+# through_options.
+# Using through_options.model_class, lookup source_name in
+# assoc_options; call this source_options.
+# Why can we not lookup source_name in self.class.assoc_options?
+# Once you have these two sets of options, it's time to
+#  write the query. Look at the above sample query to inspire
+# your building of the query from the constituent association options.
+#
+# Unlike when you used where in the belongs_to/has_many, you'll
+# have to ::parse_all yourself.
   end
 end
